e5ccb4051d04fb574e0a01ae09779d951f6c85ab
GarlicBuns19 <planterx26@gmail.com>
2022-08-23 11:41:16 +0200

    Delete

--- index.js
+++ index.js
@@ -10,107 +10,114 @@ const bodyParser = require("body-parser");
 const port = parseInt(process.env.PORT) || 4000;
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
 app.use(
-  express.json(),
-  cors(),
-  router,
-  express.urlencoded({
-    extended: true,
-  })
+    express.json(),
+    cors(),
+    router,
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 app.listen(port, (err) => {
-  if (err) throw err;
-  console.log(`Sever is running on http://localhost:${port}`);
+    if (err) throw err;
+    console.log(`Sever is running on http://localhost:${port}`);
 });
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views", "/index.html"));
+    res.sendFile(path.join(__dirname, "./views", "/index.html"));
 });
 
 router.get("/error", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views", "/404.html"));
+    res.sendFile(path.join(__dirname, "./views", "/404.html"));
 });
 
 // ================== Product data ==================== //
 
+// Get all products
 router.get("/products", (req, res) => {
-  let gettingProducts = `Select * from products;`;
+    let gettingProducts = `Select * from products;`;
 
-  db.query(gettingProducts, (err, products) => {
-    if (err) {
-      res.redirect("/error");
-      console.log(err);
-    } else {
-      res.json({
-        status: 200,
-        results: products,
-      });
-    }
-  });
+    db.query(gettingProducts, (err, products) => {
+        if (err) {
+            res.redirect("/error");
+            console.log(err);
+        } else {
+            res.json({
+                status: 200,
+                results: products,
+            });
+        }
+    });
 });
 
+// Get Single Product
 router.get(`/products/:id`, (req, res) => {
-  let getSingleProduct = `Select * from products Where gpu_id = ${req.params.id};`;
+    let getSingleProduct = `Select * from products Where gpu_id = ${req.params.id};`;
 
-  db.query(getSingleProduct, (err, product) => {
-    if (err) {
-      res.redirect("/error");
-      console.log(err);
-    } else if (product == "" || null) {
-      res.redirect("/error");
-      console.log(err);
-    } else {
-      res.json({
-        status: 200,
-        results: product,
-      });
-    }
-  });
+    db.query(getSingleProduct, (err, product) => {
+        if (err) {
+            res.redirect("/error");
+            console.log(err);
+        } else if (product == "" || null) {
+            res.redirect("/error");
+            console.log(err);
+        } else {
+            res.json({
+                status: 200,
+                results: product,
+            });
+        }
+    });
 });
 
+// Add product
 router.post("/products", bodyParser.json(), (req, res) => {
-  let {
-    gpuNoA,
-    gpuNrAr,
-    gpuGen,
-    gpuChip,
-    released,
-    memoryGb,
-    memoryType,
-    memoryBit,
-    gpuClock,
-    memoryClock,
-  } = req.body;
-  let newProduct = `Insert into products(gpuNoA,gpuNrAr,gpuGen,gpuChip,released,memoryGb,memoryType,memoryBit,gpuClock,memoryClock)
+    let {
+        gpuNoA,
+        gpuNrAr,
+        gpuGen,
+        gpuChip,
+        released,
+        memoryGb,
+        memoryType,
+        memoryBit,
+        gpuClock,
+        memoryClock,
+    } = req.body;
+    let newProduct = `Insert into products(gpuNoA,gpuNrAr,gpuGen,gpuChip,released,memoryGb,memoryType,memoryBit,gpuClock,memoryClock)
                     Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
 
-  db.query(newProduct, [
-    gpuNoA,
-    gpuNrAr,
-    gpuGen,
-    gpuChip,
-    released,
-    memoryGb,
-    memoryType,
-    memoryBit,
-    gpuClock,
-    memoryClock,
-  ],(err,newProduct) => {
-    if(err) throw err
-    console.log(newProduct.affectedRows)
-  });
+    db.query(newProduct, [
+        gpuNoA,
+        gpuNrAr,
+        gpuGen,
+        gpuChip,
+        released,
+        memoryGb,
+        memoryType,
+        memoryBit,
+        gpuClock,
+        memoryClock,
+    ], (err, newProduct) => {
+        if (err) {
+            res.redirect("/error");
+            console.log(err);
+        }
+        console.log(newProduct.affectedRows)
+    });
 });
 
-router.put('/products/:id',bodyParser.json(),(req,res) => {
-    let{
+// Edit product
+router.put('/products/:id', bodyParser.json(), (req, res) => {
+    let {
         gpuNoA,
         gpuNrAr,
         gpuGen,
@@ -135,7 +142,7 @@ router.put('/products/:id',bodyParser.json(),(req,res) => {
         memoryClock = ?
         Where gpu_id = ${req.params.id}
     `
-    db.query(editProduct,[
+    db.query(editProduct, [
         gpuNoA,
         gpuNrAr,
         gpuGen,
@@ -146,9 +153,19 @@ router.put('/products/:id',bodyParser.json(),(req,res) => {
         memoryBit,
         gpuClock,
         memoryClock,
-    ],(err,editedProduct) => {
-        if(err) throw err
+    ], (err, editedProduct) => {
+        if (err) {
+            res.redirect("/error");
+            console.log(err);
+        }
         res.end(JSON.stringify(editedProduct))
+    })
+})
+
+router.delete('/products/:id',(req, res) => {
+    let deleteProduct = `delete from products where gpu_id = ${req.params.id}`
+    db.query(deleteProduct,(err) => {
+        if(err) throw err
     })
 })
 // ========================================================= //
