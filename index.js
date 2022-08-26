b9e3452b958867573e0fdb4a7bf02f4f90c8bc43
GarlicBuns19 <planterx26@gmail.com>
2022-08-26 08:51:35 +0200

    All

--- index.js
+++ index.js
@@ -34,16 +34,16 @@ app.listen(port, (err) => {
     if (err) throw err;
     console.log(`Sever is running on http://localhost:${port}`);
 });
-router.get("/*", (req, res) => res.status(404).redirect("/error"))
-
-router.get("/error", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views", "/404.html"));
-});
 
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views", "/index.html"));
 });
 
+router.get("/*", (req, res) => res.status(404).redirect("/error"))
+
+router.get("/error", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views", "/404.html"));
+});
 // router.get("/register", (req, res) => {
 //     res.sendFile(path.join(__dirname, "./views", "/register.html"));
 // });
