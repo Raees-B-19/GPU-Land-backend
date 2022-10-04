47ecd3ff9682fe3308149dc5de1070a330404caa
GarlicBuns19 <planterx26@gmail.com>
2022-10-04 13:15:16 +0200

    Reset code

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(port, (err) => {
     console.log(`Sever is running on http://localhost:${port}`);
 });
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public", "/index.html"));
 });
 
